49907c1760068e604276169fd2ca571e6a9c8cdf
rvhirsch <rhirsch@pugetsound.edu>
2017-01-18 23:53:34 -0800

    main 3 classes - to be filled out

---
+++ src/Attribute.java
@@ -0,0 +1,10 @@
+/**
+ * Created by rvhirsch on 1/18/17.
+ */
+
+public class Attribute {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Tuple.java
@@ -0,0 +1,10 @@
+/**
+ * Created by rvhirsch on 1/18/17.
+ */
+
+public class Tuple {
+
+    public static void main(String[] args) {
+
+    }
+}
