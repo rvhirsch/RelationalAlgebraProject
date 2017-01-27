1011c409a359fd69efa69c4092ed1ba984ebe8c0
Jwag598 <joshsemail598@gmail.com>
2017-01-27 13:43:05 -0800

    Added file input to populate database and toSTring for database

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,17 @@
 /**
  * Created by rvhirsch on 1/24/17.
  */
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
-    private static ArrayList<Rel> tables =  new ArrayList<Rel>();
+    private ArrayList<Rel> tables =  new ArrayList<Rel>();
 
     /**
      * Adds a table to the database
@@ -32,71 +35,58 @@ public class Main {
      */
     public String toString() {
         String toReturn = "";
-        for (int x = 0; x < this.tables.size(); x++) {
-            toReturn+=this.tables.get(x).getName() + "\n";
-            toReturn+=this.tables.get(x).toString() + "\n";
+        for (int x = 0; x < tables.size(); x++) {
+            toReturn+= tables.get(x).getName() + "\n";
+            toReturn+= tables.get(x).toString() + "\n";
         }
-        toReturn+="\n\n";
         return toReturn;
     }
 
-    public static boolean populateDatabase (File file) throws IOException {
-        boolean finished = false;
+    public void populateDatabase (File file) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(file));
+        String curLine = "";
 
-        String curLine = null;
-
-        //load the first line into the reader and check if its null
         while ((curLine = br.readLine()) != null) {
-            Rel newRel = new Rel(curLine);
+            Rel rel = new Rel(curLine);
 
             curLine = br.readLine();
             String categories[] = curLine.split(" ");
 
-            while ((curLine = br.readLine()) != "*") {
+            curLine = br.readLine();
+            while (curLine.contains("*") == false && curLine != null) {
                 Tup tup = new Tup();
+                //System.out.println(curLine);
+                String[] entries = curLine.split(" ");
 
-                //Get the name of the table, 1 word on 1 line
-                //get the name of the categories,
-                //load the line containing categories
-                //parse the line into an array, elements seperated by a space
-                //load the next line containing attributes and parse it by space
-                //the nth attribute has a col name of the nth element from the earlier array
-                //end this loop when we hit a "*"
-
-                String entries[] = curLine.split(" ");
-
-                for (int y = 0; y < entries.length; y++) {
-                    Attr attr = new Attr(entries[y], categories[y]);
+                for (int x = 0; x < entries.length; x++) {
+                    Attr attr = new Attr(entries[x], categories[x]);
                     tup.addAttr(attr);
                 }
+
                 try {
-                    newRel.insert(tup);
+                    rel.insert(tup);
                 } catch (IllegalInsertException e) {
-                    System.out.println(e.getMessage());
+                    e.printStackTrace();
                 }
+                curLine = br.readLine();
             }
-            tables.add(newRel);
+            tables.add(rel);
         }
-
-        br.close();
-
-        return finished;
     }
 
 
     public static void main(String[] args) throws IOException{
-        File file = new File("C:\\Users\\Josh\\Documents\\Capstone Project\\RelationalAlgebraProject\\src\\SimpleInput.txt");
+        File file = new File("files\\SimpleInput.txt");
+        Main main = new Main();
         System.out.println("Starting...");
         System.out.println();
 
         //String test = "this is a test 8 9 89";
         //System.out.println(Arrays.toString(test.split(" ")));
 
-        populateDatabase(file);
+        main.populateDatabase(file);
+        System.out.println(main.toString());
 
-
-        System.out.println();
         System.out.println("Ended!");
     }
 }
