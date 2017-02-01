55dc937e58739c2250d55abea9fc8aa3270fb41e
Jwag598 <joshsemail598@gmail.com>
2017-02-01 13:51:59 -0800

    more tree shtuff

--- src/InputTree.java
+++ src/InputTree.java
@@ -2,8 +2,10 @@
  * Created by Josh on 1/30/2017.
  */
 
+import java.util.ArrayList;
+
 public class InputTree {
-    public class InputTreeNode {
+    public static class InputTreeNode {
         private boolean operator;   //True if the node is an operator, false if it is a relation
         private String name;    //the name of the relation, or the name of the operator
         private String conditional; //if node is an operator and needs a condition, you put this here
@@ -44,6 +46,16 @@ public class InputTree {
             this.root = null;
             this.leftChild = null;
             this.rightChild = null;
+        }
+
+        public InputTreeNode () {
+            this.operator = false;
+            this.name = null;
+            this.conditional = null;
+            this.root = null;
+            this.leftChild = null;
+            this.rightChild = null;
+            this.rel = null;
         }
 
         public String getName() {
@@ -102,4 +114,83 @@ public class InputTree {
             this.rightChild = node;
         }
     }
+
+    private InputTreeNode root;
+
+    public InputTree() {
+        this.root = null;
+    }
+
+    public void addNode(InputTreeNode node) {
+        if (this.root == null) {
+            this.root = node;
+        } else {
+            InputTreeNode temp = this.root;
+
+            this.root = node;
+            this.root.setRight(temp);
+            temp.setRoot(this.root);
+        }
+    }
+
+    public InputTreeNode getRoot() {
+        return root;
+    }
+
+    public String toString() {
+        return this.toStringHelper(this.root);
+    }
+
+    private String toStringHelper(InputTreeNode node) {
+        String toReturn = node.getName() + "\n";
+        if (node.getLeft() != null) {
+            toReturn += "L: " + toStringHelper(node.getLeft());
+        }
+
+        if (node.getRight() != null) {
+            toReturn += "R: " + toStringHelper(node.getRight());
+        }
+
+        return toReturn;
+    }
+
+
+    public void read(String str) {
+        String[] symbols = {"Π","σ","X"};
+        String[] line = str.split(" ");
+
+
+    }
+
+
+    public static void main(String[] args) {
+        InputTree tree = new InputTree();
+
+//        InputTreeNode nodeA = new InputTreeNode("Project");
+//        InputTreeNode nodeB = new InputTreeNode("Select");
+//        InputTreeNode nodeC = new InputTreeNode("Join");
+//        InputTreeNode nodeD = new InputTreeNode("RelOne");
+//        InputTreeNode nodeE = new InputTreeNode("RelTwo");
+//
+//        nodeA.setRight(nodeB);
+//        nodeB.setRight(nodeC);
+//        nodeC.setRight(nodeE);
+//        nodeC.setleft(nodeD);
+
+//        tree.addNode(nodeA);
+
+
+
+
+
+
+
+
+
+        String test = "Π penis σ (A<18) X R1 R2";
+        tree.read(test);
+
+        System.out.println(tree.toString());
+    }
+
 }
