f556dc6b9611ed0b5558ecaf70c15126f4bdc04a
rvhirsch <rhirsch@pugetsound.edu>
2017-04-02 15:23:49 -0600

    a bunch of new stuff - only half really works but w'ev

--- src/Parser.java
+++ src/Parser.java
@@ -1,8 +1,6 @@
 public class Parser {
 
-
     public Parser() {
-
     }
 
     public String parseLatexToString (String input) {
@@ -14,9 +12,13 @@ public class Parser {
         for (int x = 0; x < input.length(); x++) {
             curWord += input.charAt(x);
 
-            if (input.charAt(x) == '\\') {
-                curWord = "";
+            if (curWord.equals("max")) {        // TODO: FINISH THIS
+                statement += "max";
             }
+
+//            if (input.charAt(x) == '\\') {
+//                curWord = "";
+//            }
 
             if (input.charAt(x) == ' ') {
                 statement += curWord.toLowerCase();
@@ -37,93 +39,93 @@ public class Parser {
                 curWord = "";
             }
             else if (input.charAt(x) == '}' && (input.contains("<") || input.contains(">") || input.contains("="))) {
-                statement += curWord.replace('}', '\u0000').toLowerCase();
+                statement += " " + curWord.replace('}', '\u0000').toLowerCase();
                 curWord = "";
             }
             else if (input.charAt(x) == '}') {
-                statement += curWord.replace('}', '\u0000').toLowerCase();
+                statement += " " + curWord.replace('}', '\u0000').toLowerCase();
                 curWord = "";
             }
 
             switch (curWord) {
-                case ("Pi"):       // project
+                case ("\\Pi"):       // project
                     if (input.charAt(x + 1) != ' ' && input.charAt(x + 1) == '_') {
                         statement += "PI ";
                         curWord = "";
                     }
 
                     break;
-                case ("sigma"):    // select
+                case ("\\sigma"):    // select
                     statement += "SIGMA ";
                     curWord = "";
 
                     break;
-                case ("G"):
+                case ("\\G"):
                     statement += "GROUP ";
                     curWord = "";
 
                     break;
-                case ("bigtimes"):  // cross product
+                case ("\\bigtimes"):  // cross product
                     statement += "CROSSJOIN ";
                     curWord = "";
 
                     break;
-                case ("bowtie"):    // natural join
+                case ("\\bowtie"):    // natural join
                     statement += "NATJOIN ";
                     curWord = "";
 
                     break;
-                case ("rho"):      // rename    TODO
+                case ("\\rho"):      // rename    TODO
                     statement += "RENAME ";
                     curWord = "";
 
                     break;
-                case ("cup"):     // union
+                case ("\\cup"):     // union
                     statement += "UNION ";
                     curWord = "";
 
                     break;
-                case ("cap"):   // intersection
+                case ("\\cap"):   // intersection
                     statement += "INTERSECT ";
                     curWord = "";
 
                     break;
-                case ("vee"):   // and
+                case ("\\vee"):   // and
                     statement += "AND ";
                     curWord = "";
 
                     break;
-                case ("land"):   // and - 2nd possibility
+                case ("\\land"):   // and - 2nd possibility
                     statement += "AND ";
                     curWord = "";
 
                     break;
-                case("wedge"):  // or
+                case("\\wedge"):  // or
                     statement += "OR ";
                     curWord = "";
 
                     break;
-                case("lor"):  // or - 2nd possibility
+                case("\\lor"):  // or - 2nd possibility
                     statement += "OR ";
                     curWord = "";
 
                     break;
-                case("-"):  // or - 2nd possibility     TODO
+                case("\\-"):  // or - 2nd possibility     TODO
                     statement += "EXCEPT ";
                     curWord = "";
 
                     break;
-                case ("leftouterjoin"):
+                case ("\\leftouterjoin"):
                     statement += "LOJ ";
                     curWord = "";
 
                     break;
-                case ("rightouterjoin"):
+                case ("\\rightouterjoin"):
                     statement += "ROJ ";
                     curWord = "";
 
                     break;
-                case ("fullouterjoin"):
+                case ("\\fullouterjoin"):
                     statement += "FOJ ";
                     curWord = "";
 
@@ -143,8 +145,9 @@ public class Parser {
 
         String from = "";   // table name
         String where = "";  // where clause
-        String cols = "";
-        String group = "";
+        String cols = "";   // cols to select
+        String group = "";  // stuff to group by
+        String aggr = "";   // aggregation statements - i.e. cols to select when grouping
 
         String comm;    // current command in list
         for (int i=0; i<commands.length; i++) {
@@ -238,24 +241,14 @@ public class Parser {
 
                         i += from.split(" ").length + 1;
 
-
                         sql += "WHERE " + where;
-
-                        if (!group.equals("")) {
-                            sql += "GROUP BY " + group;
-                        }
                     }
                     else {
-                        sql += "SELECT * FROM ";
+                        sql += "(SELECT * FROM ";
                         from = getFrom(commands, i);
 
                         i += from.split(" ").length + 1;
                         sql += from;
-
-                        if (!group.equals("")) {
-                            sql = sql.substring(0, sql.length()-1);
-                            sql += " GROUP BY " + group;
-                        }
                     }
 
 //                    System.out.println("GROUP: " + group);
@@ -265,9 +258,28 @@ public class Parser {
 
                     break;
                 case ("GROUP"):
+                    String grpCols = getCols(commands, i);
                     group = getGroup(commands, i);
+                    i += grpCols.split(" ").length;
+                    String grpFrom = parseStringToSQL(getFrom(commands, i));
+
+//                    group = getGroup(commands, i);
+//                    i += group.split(" ").length;
+//
+//                    while (group.charAt(group.length()-1) == ',') {
+//                        group += getGroup(commands, i);
+//                    }
 //                    System.out.println("GRP: " + group + "\n");
-                    i += group.split(" ").length;
+
+
+                    System.out.println("COLS: " + grpCols);
+                    System.out.println("FROM: " + grpFrom);
+                    System.out.println("GROUP: " + group);
+
+                    sql += "SELECT " + grpCols + " FROM " + grpFrom;
+                    if (group.length() > 0) {
+                        sql += " GROUP BY " + group;
+                    }
 
                     break;
                 case ("UNION"):
@@ -319,6 +331,7 @@ public class Parser {
 
         }
 
+        sql = sql.replace("\u0000", "");    // replace special space character
         return sql;
     }
 
@@ -362,20 +375,46 @@ public class Parser {
 
     private String getGroup(String[] comms, int i) {
         String group = "";
-        for (int j=i+1; j<comms.length; j++) {
+        group = comms[i-1] + " " + group;
+        for (int j=i-1; j>=0; j--) {
             if (comms[j].contains(",")) {
-                group += comms[j] + " ";
+                group = comms[j] + " " + group;
             }
-            if (!comms[j].contains((","))) {
-                group += comms[j];
+            else if (comms[j].contains("(")) {
+                group = comms[j] + " " + group;
+                return group;
+            }
+            else if (j == 0) {
+                group = comms[j] + " " + group;
                 return group;
             }
         }
+//        for (int j=i+1; j<comms.length; j++) {
+//            System.out.println("C[j] = " + comms[j]);
+//            if (comms[j].contains(",")) {
+//                group += comms[j] + " ";
+//            }
+//            else if (!comms[j].contains((",")) || comms[j].equals("max")) {
+//                group += comms[j] + comms[j+1];
+//                return group;
+//            }
+//        }
         return group;
+    }
+
+    public String runTest(String input, int num, Parser p) {
+        System.out.println("\nLaTeX Input " + num + ": " + input);
+        String latexToStr = p.parseLatexToString(input);
+        System.out.println("String Output: " + latexToStr);
+        String strToSql = p.parseStringToSQL(latexToStr);
+        System.out.println("SQL Output: " + strToSql);
+        return strToSql;
     }
 
     public static void main(String[] args) throws Exception {
         Parser p = new Parser();
+
+        // SELECT name FROM (Person)
         String sampleInput1 = "\\Pi_{name}(Person)";
 
         // SELECT name, age  FROM (SELECT * FROM person INNER JOIN eats ) WHERE age > 16
@@ -384,44 +423,25 @@ public class Parser {
         // SELECT * FROM (person INNER JOIN eats ) WHERE age > 10 OR name == 'sally' 
         String sampleInput3 = "\\sigma_{age > 10 || name == 'sally'}(Person \\bowtie Eats)";    // WORKS
 
-        // SELECT * FROM (person NATURAL JOIN eats) group by age
-        String sampleInput4 = "\\G_{name, age}(\\sigma(Person \\bowtie Eats))";    // WORKS
-//        String sampleInput4 = "\\sigma(Person \\bowtie Eats)";    // WORKS
+        // SELECT max(age) FROM (person INNER JOIN eats) GROUP BY age, name
+        String sampleInput4 = "_{age, name}\\G_{max(age)} (Person \\bowtie Eats)";    // WORKS
 
         // SELECT name, age  FROM (SELECT * FROM (eats INNER JOIN (person INTERSECT pizzeria ) ) ) WHERE age > 10 OR name == ' sally'
-        String sampleInput5 = "\\Pi_{name, age}(\\sigma_{age > 10 || name == 'sally'}(Eats \\bowtie (Person \\cap Pizzeria)))";    // WORKS
+        String sampleInput5 = "\\Pi_{name, age}(\\sigma_{age > 10 || name == 'sally'}(Eats \\bowtie (Person \\cap Pizzeria)))";
+
+        // SELECT * FROM (person)
+        String sampleInput6 = "\\sigma(Person)";
 
         String latexToStr;
         String strToSql;
 
-//        System.out.println("LaTeX Input 1: " + sampleInput1);
-//        latexToStr = p.parseLatexToString(sampleInput1);
-//        System.out.println("String Output: " + latexToStr);
-//        strToSql = p.parseStringToSQL(latexToStr);
-//        System.out.println("SQL Output: " + strToSql);
+//        p.runTest(sampleInput1, 1, p);
+//        p.runTest(sampleInput2, 2, p);
+//        p.runTest(sampleInput3, 3, p);
 
-//        System.out.println("\nLaTeX Input 2: " + sampleInput2);
-//        latexToStr = p.parseLatexToString(sampleInput2);
-//        System.out.println("String Output: " + latexToStr);
-//        strToSql = p.parseStringToSQL(latexToStr);
-//        System.out.println("SQL Output: " + strToSql);
+        p.runTest(sampleInput4, 4, p);      // TODO: STILL TOTALLY BROKEN
 
-        System.out.println("\nLaTeX Input 3: " + sampleInput3);
-        latexToStr = p.parseLatexToString(sampleInput3);
-        System.out.println("String Output: " + latexToStr);
-        strToSql = p.parseStringToSQL(latexToStr);
-        System.out.println("SQL Output: " + strToSql);
-
-        System.out.println("\nLaTeX Input 4: " + sampleInput4);
-        latexToStr = p.parseLatexToString(sampleInput4);
-        System.out.println("String Output: " + latexToStr);
-        strToSql = p.parseStringToSQL(latexToStr);
-        System.out.println("SQL Output: " + strToSql);
-
-//        System.out.println("\nLaTeX Input 5: " + sampleInput5);
-//        latexToStr = p.parseLatexToString(sampleInput5);
-//        System.out.println("String Output: " + latexToStr);
-//        strToSql = p.parseStringToSQL(latexToStr);
-//        System.out.println("SQL Output: " + strToSql);
+//        p.runTest(sampleInput5, 5, p);
+//        p.runTest(sampleInput6, 6, p);
     }
 }
