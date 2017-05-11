85868e255c567274c1b93185af45d522e147fc0d
rvhirsch <rhirsch@pugetsound.edu>
2017-05-10 19:43:42 -0700

    updated tests

--- test/ParserTest3.java
+++ test/ParserTest3.java
@@ -3,15 +3,9 @@ import junit.framework.TestCase;
 public class ParserTest3 extends TestCase {
     Parser3 p;
     DBInfo d;
-    String sampleInput1 = "\\Pi_{name}(Person)";
-    String sampleInput2 = "\\Pi_{name, age}(\\sigma_{age > 16}(Person \\bowtie Eats))";
-    String sampleInput3 = "\\sigma_{age > 10 || name == 'sally'}(Person \\bowtie Eats)";
-    String sampleInput4 = "_{age, name}\\gamma_{max(age), name} (Person \\bowtie Eats)";
-    String sampleInput5 = "\\Pi_{name, age}(\\sigma_{age > 10 || name == 'sally'}(Eats \\bowtie (Person \\cap Pizzeria)))";
-    String sampleInput6 = "\\sigma(Person)";
-    String sampleInput7 = "_{age}\\Pi_{name}(Person)";
 
     public void testCase1() throws Exception {
+        String sampleInput1 = "\\Pi_{name}(Person)";
         p = new Parser3(sampleInput1, d);
 
         String test = p.runTest(sampleInput1, 1, p);
@@ -21,43 +15,47 @@ public class ParserTest3 extends TestCase {
     }
 
     public void testCase2() throws Exception {
+        String sampleInput2 = "\\Pi_{name, age}(\\sigma_{age > 16}(Person \\bowtie Eats))";
         p = new Parser3(sampleInput2, d);
 
         String test = p.runTest(sampleInput2, 2, p);
-        String answer = "SELECT name, age FROM (SELECT * FROM (Person INNER JOIN Eats) WHERE age > 16)";
+        String answer = "SELECT name, age FROM (SELECT * FROM Person NATURAL JOIN Eats WHERE age > 16)";
 
         assertEquals(test, answer);
     }
 
     public void testCase3() throws Exception {
+        String sampleInput3 = "\\sigma_{age > 10 || name == 'sally'}(Person \\bowtie Eats)";
         p = new Parser3(sampleInput3, d);
 
         String test = p.runTest(sampleInput3, 3, p);
-        String answer = "SELECT * FROM (Person INNER JOIN Eats) WHERE age > 10 OR name == 'sally'";
+        String answer = "SELECT * FROM Person NATURAL JOIN Eats WHERE age > 10 OR name == 'sally'";
 
         assertEquals(test, answer);
     }
 
     public void testCase4() throws Exception {
+        String sampleInput4 = "_{age, name}\\gamma_{max(age), name} (Person \\bowtie Eats)";
         p = new Parser3(sampleInput4, d);
 
         String test = p.runTest(sampleInput4, 4, p);
-        String answer = "SELECT max(age), name FROM Person INNER JOIN Eats GROUP BY age, name";
+        String answer = "SELECT max(age), name FROM (Person NATURAL JOIN Eats) GROUP BY age, name";
 
         assertEquals(test, answer);
     }
 
     public void testCase5() throws Exception {
+        String sampleInput5 = "\\Pi_{name, age}(\\sigma_{age > 10 || name == 'sally'}(Eats \\bowtie (Person \\cap Pizzeria)))";
         p = new Parser3(sampleInput5, d);
 
         String test = p.runTest(sampleInput5, 5, p);
-//        String answer = "SELECT name, age FROM (SELECT * FROM (eats INNER JOIN (person INTERSECT pizzeria))) WHERE age > 10 OR name == 'sally' ";
-        String answer = "SELECT name, age FROM (SELECT * FROM (Eats INNER JOIN (Person INTERSECT Pizzeria)) WHERE age > 10 OR name == 'sally')";
+        String answer = "SELECT name, age FROM (SELECT * FROM Eats NATURAL JOIN (Person INTERSECT Pizzeria) WHERE age > 10 OR name == 'sally')";
 
         assertEquals(test, answer);
     }
 
     public void testCase6() throws Exception {
+        String sampleInput6 = "\\sigma(Person)";
         p = new Parser3(sampleInput6, d);
 
         String test = p.runTest(sampleInput6, 6, p);
@@ -67,10 +65,31 @@ public class ParserTest3 extends TestCase {
     }
 
     public void testCase7() throws Exception {
+        String sampleInput7 = "_{age}\\Pi_{name}(Person)";
         p = new Parser3(sampleInput7, d);
 
         String test = p.runTest(sampleInput7, 7, p);
         String answer = "SELECT name FROM Person GROUP BY age";
+
+        assertEquals(test, answer);
+    }
+
+    public void testCase8() throws Exception {
+        String sampleInput8 = "_{age}\\Pi_{name}(Person)";
+        p = new Parser3(sampleInput8, d);
+
+        String test = p.runTest(sampleInput8, 8, p);
+        String answer = "SELECT name FROM Person GROUP BY age";
+
+        assertEquals(test, answer);
+    }
+
+    public void testCase9() throws Exception {
+        String sampleInput9 = "\\sigma(Person \\foj Eats)";
+        p = new Parser3(sampleInput9, d);
+
+        String test = p.runTest(sampleInput9, 9, p);
+        String answer = "SELECT * FROM Person LEFT JOIN Eats UNION SELECT * FROM Person RIGHT JOIN Eats";
 
         assertEquals(test, answer);
     }
